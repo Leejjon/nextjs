d1b6410a1229b2571496550b743a7c2810561977
Leejjon <leejjon@gmail.com>
2023-06-09 11:22:50 +0200

    The vercel build complains.

--- src/pages/api/counter/[entryId].ts
+++ src/pages/api/counter/[entryId].ts
@@ -4,7 +4,7 @@ import {NextApiRequest, NextApiResponse} from "next";
 export default async function handler(req: NextApiRequest,
                                       res: NextApiResponse) {
     const {entryId} = req.query;
-    const {rows} = await sql`SELECT COUNT(*) FROM queueEntries WHERE entryId < ${entryId} AND ready = false;`;
+    const {rows} = await sql`SELECT COUNT(*) FROM queueEntries WHERE entryId < ${entryId as string} AND ready = false;`;
     res.setHeader('Cache-Control', 'public, max-age=0');
     let returnValue = 0;
     if (rows) {
