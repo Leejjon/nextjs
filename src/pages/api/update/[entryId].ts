1cb01a3a6b81032c3e9dd29eb485f08eeb5221a5
Leejjon <leejjon@gmail.com>
2023-06-09 11:23:54 +0200

    Also fixed the other endpoint.

--- src/pages/api/update/[entryId].ts
+++ src/pages/api/update/[entryId].ts
@@ -5,7 +5,7 @@ export default async function handler(req: NextApiRequest,
                                       res: NextApiResponse) {
     if (req.method === "POST") {
         const {entryId} = req.query;
-        await sql`UPDATE queueEntries SET ready = true WHERE entryid = ${entryId}`;
+        await sql`UPDATE queueEntries SET ready = true WHERE entryid = ${entryId as string}`;
 
         res.status(200).json({});
     } else {
